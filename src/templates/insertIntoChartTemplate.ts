96057506c2454606729299a5da0d7d6d37272620
HendrikThePendric <hendrik@dhis2.org>
2023-11-01 16:16:05 +0100

    fix: use correct classname in html template

--- src/templates/insertIntoChartTemplate.ts
+++ src/templates/insertIntoChartTemplate.ts
@@ -1,6 +1,6 @@
 import { parseDashboardItemTemplate } from './parseTemplate'
 
-const template = `<img class="map" src="data:image/png;base64,{{base64Str}}"></img>`
+const template = `<img class="chart" src="data:image/png;base64,{{base64Str}}"></img>`
 
 export const insertIntoChartTemplate = (name: string, base64Str: string) =>
     parseDashboardItemTemplate(template, { name, base64Str })
