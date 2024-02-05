9c81e74587834ad32d00f607969a30db86b317de
HendrikThePendric <hendrik@dhis2.org>
2024-02-05 14:24:27 +0100

    fix: add generic templates for base64 images and other output

---
+++ src/templates/insertIntoImage.ts
@@ -0,0 +1,6 @@
+import { parseDashboardItemTemplate } from './parseTemplate'
+
+const template = `<img class="visualization-image" src="data:image/png;base64,{{base64Str}}"></img>`
+
+export const insertIntoImage = (base64Str: string, name: string = '') =>
+    parseDashboardItemTemplate(template, { name, base64Str })
