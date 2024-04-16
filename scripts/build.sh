3e87db8327963c95a48221fe3c0c79369e90029b
HendrikThePendric <hendrik@dhis2.org>
2024-04-16 10:29:36 +0200

    fix: add version to artefact name

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,10 @@
 
 # Note: showing the output of various commands is confusing so output
 # is surpressed by piping to >/dev/null 2>&1
-echo "arg 1 $1"
-echo "version var $version"
+
+VERSION="${1:-latest}"
+FILE_NAME=push-analytics-$VERSION.tar.gz
+
 # Clear previous builds
 rimraf ./dist && mkdir ./dist && mkdir ./dist/images
 # Copy package.jsnon and lockfile to dist
@@ -15,10 +17,10 @@ cd ..
 # Compile TypeScript to JavaScript (suppress output)
 npx -y tsc -p ./tsconfig.prod.json ./dist >/dev/null 2>&1
 # Compress the ./dist folder (suppress output)
-tar -zcvf ./push-analytics.tar.gz ./dist ./dist >/dev/null 2>&1
+tar -zcvf ./$FILE_NAME ./dist ./dist >/dev/null 2>&1
 # Colocate the compressed file with the uncompressed assets
-mv ./push-analytics.tar.gz ./dist/push-analytics.tar.gz
+mv ./$FILE_NAME ./dist/$FILE_NAME
 # Compute the file size
-FILE_SIZE_KB=$(du -k ./dist/push-analytics.tar.gz | cut -f1)
+FILE_SIZE_KB=$(du -k ./dist/$FILE_NAME | cut -f1)
 FILE_SIZE_MB=$(bc <<<"scale=2; $FILE_SIZE_KB/1024")
 echo "Created build artifact './dist/push-analytics.tar.gz' ("$FILE_SIZE_MB" MB)"
