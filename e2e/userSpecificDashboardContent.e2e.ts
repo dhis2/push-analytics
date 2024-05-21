bb5a9787ecf71cef6955b0e5af8ad700e7d6c8ab
HendrikThePendric <hendrik@dhis2.org>
2024-05-21 13:55:23 +0200

    chore: assert generated HTML is similar rather than identical to fixtures

--- e2e/userSpecificDashboardContent.e2e.ts
+++ e2e/userSpecificDashboardContent.e2e.ts
@@ -2,6 +2,7 @@ import fs from 'node:fs'
 import path from 'node:path'
 import assert from 'node:assert'
 import { describe, test } from 'node:test'
+import stringSimilarity from 'string-similarity'
 import request from 'supertest'
 
 /* Before writing the HTML strings to the fixtures files
@@ -18,6 +19,7 @@ describe('producing user specific dashboard content ()', () => {
         throw new Error('HOST and PORT env variables missing, aborting test run')
     }
     const url = `${process.env.HOST}:${process.env.PORT}`
+    console.log(`Running tests agains URL "${url}"`)
     const req = request(url)
     const dashboardId = 'KQVXh5tlzW2'
     const usernameWithNorwegianLocale = 'test_user_national_nb'
@@ -44,7 +46,21 @@ describe('producing user specific dashboard content ()', () => {
             // Enable line below to generate new fixtures
             // fs.writeFileSync(filePath, actualHtml)
             htmlPerUser.set(username, actualHtml)
-            assert.strictEqual(actualHtml, expectedHtml)
+
+            /* The e2e dashboard used to assert content customisation looks slightly different
+             * depending on when it is viewed. As a result the HTML strings are not constant.
+             * They are very similar each time though. For now the test only asserts that the
+             * actual HTML and the expected HTML are 75% "similar". I expect this test to be good
+             * enough to catch regressions, even though "being similar to a fixture" may not always
+             * be equivalent to "generating the correct output". If in the furure it turns out that
+             * we need more grnaular tests, we'll either have to write regex based tests or mount the
+             * generated HTML to JS DOM, to assert the tables have expected content and the images
+             * are showing.*/
+            const similarity = stringSimilarity.compareTwoStrings(
+                actualHtml,
+                expectedHtml
+            )
+            assert.strictEqual(similarity > 0.75, true)
         })
     }
 
