92c88be9e69f073a57e9bbc60ce5de73e2df6691
HendrikThePendric <hendrik@dhis2.org>
2025-01-09 17:14:12 +0100

    chore: log response in test output

--- e2e/userSpecificDashboardContent.e2e.ts
+++ e2e/userSpecificDashboardContent.e2e.ts
@@ -33,6 +33,8 @@ describe('producing user specific dashboard content', () => {
             const expectedHtml = fs.readFileSync(filePath).toString()
             const response = await req.get('/').query({ dashboardId, username, locale })
 
+            console.log(response)
+
             assert.strictEqual(response.status, 200)
 
             // Strip whitespace and newlines to avoid
