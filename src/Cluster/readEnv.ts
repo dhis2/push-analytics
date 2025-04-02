0425410558b7178d46729e3ea63c61b98a615e69
HendrikThePendric <hendrik@dhis2.org>
2025-04-02 23:08:22 +0200

    chore: cleanup readEnv

--- src/Cluster/readEnv.ts
+++ src/Cluster/readEnv.ts
@@ -33,7 +33,7 @@ function readEnvVariable(name: EnvVariableName): string | NodeEnvContext | LogLe
 }
 
 export function readEnv(): PushAnalyticsEnvVariables {
-    const resolvedEnd = {
+    return {
         host: readEnvVariable('HOST'),
         port: readEnvVariable('PORT'),
         baseUrl: readEnvVariable('DHIS2_CORE_URL'),
@@ -45,16 +45,4 @@ export function readEnv(): PushAnalyticsEnvVariables {
         nodeEnv: readEnvVariable('NODE_ENV') as NodeEnvContext,
         logLevel: readEnvVariable('LOG_LEVEL') as LogLevel,
     }
-
-    if (cluster.isPrimary) {
-        console.log(`process.pwd(): ${process.cwd()}`)
-        console.log(
-            `+++++++++\nresolved env:\n ${JSON.stringify(
-                resolvedEnd,
-                null,
-                4
-            )}\n=========`
-        )
-    }
-    return resolvedEnd
 }
