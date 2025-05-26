076d48844642d9fef81b3354b1d7285153bcada6
HendrikThePendric <hendrik@dhis2.org>
2025-05-26 13:56:19 +0200

    chore: copy from mounted dir instead of container that has exited

--- e2e/dashboardItemTypes.e2e.ts
+++ e2e/dashboardItemTypes.e2e.ts
@@ -42,7 +42,7 @@ describe('converting all types of dashboard items', () => {
         const similarity = stringSimilarity.compareTwoStrings(actualHtml, expectedHtml)
         console.log(`Actual and expected string are ${similarity * 100}% similar`)
         if (similarity <= 0.8) {
-            fs.writeFileSync(path.resolve('./generated-html'), actualHtml)
+            fs.writeFileSync(path.resolve('./e2e/generated-html'), actualHtml)
         }
         assert.strictEqual(similarity > 0.8, true)
     })
